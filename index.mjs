4c735129ecb63c9fd566912e2f4d523f2ecee267
eebbesen <eebbesen.git@gmail.com>
2020-03-23 11:29:16 -0500

    readme; pass in url

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import foodpantries_org from './modules/foodpantries_org.js'
 
-const res = foodpantries_org.scrape('https://www.foodpantries.org/ci/mn-st_paul');
+// const res = foodpantries_org.scrape('https://www.foodpantries.org/ci/mn-st_paul');
+const res = foodpantries_org.scrape(process.argv[2]);
 
 res.then(r => {
   console.log('RESULTS\n', r);
